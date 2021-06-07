9222804a4069a7fc94d98393c540693bf7171fac
rotosiri-zu <hongjitufang25@gmail.com>
2021-06-07 21:19:20 +0900

    2.6.3メソッド定義における引数の()の途中まで

--- 2.5.3.rb
+++ 2.5.3.rb
@@ -10,3 +10,53 @@ end
 #=>10より大きい
 
 country = 'italy'
+if country == 'japan'
+  puts 'こんにちは'
+elsif country == 'us'
+  puts 'Hello'
+elsif country == 'italy'
+  puts 'ciao'
+else
+  puts '???'
+end
+
+#=>ciao
+
+country = 'italy'
+
+#if文の戻り値を変数に代入
+greeting = 
+  if country == 'japan'
+    puts 'こんにちは'
+  elsif country == 'us'
+    puts 'Hello'
+  elsif country == 'italy'
+    puts 'ciao'
+  else
+    puts '???'
+  end
+
+greeting #=> "ciao"
+
+point = 7
+day = 1
+#1日であればポイント5倍
+if day == 1
+  point *=5
+end
+point #=>35
+
+point = 7
+day = 1
+#1日であればポイント5倍(if修飾子を利用)
+point *=5 if day == 1
+point #=>35
+
+country = 'italy'
+if country == 'japan'then'こんにちは'
+elsif country == 'us'then'Hello'
+elsif country == 'italy'then'ciao'
+else '???'
+end
+
+#=> "ciao"
---
+++ 2.6.rb
@@ -0,0 +1,80 @@
+#メソッドの定義
+
+def add(a,b)
+  a + b
+end
+add(1,2)#=> 3
+
+#メソッド名はスネークケースで書く
+def Hello
+  'Hello, world!'
+end
+
+#キャメルケースは使わない(エラーにはならないが一般的ではない)
+def helloWorld
+  'Hello, world!'
+end
+
+#アンダースコアでメソッド名を書き始める(アンダースコアで始まることは少ない)
+def _hello_world
+  'Hello, world!'
+end
+
+#メソッド名に数字を入れる
+def hello_world_2
+  'Hello, world!'
+end
+
+#数字から始まるメソッド名は使えない(エラーになる)
+def 2_hello_world
+  'Hello, world!'
+end  
+
+#メソッド名をひらがなにする(一般的ではない)
+def あいさつする
+  'はろー、わーるど！'
+end
+#ひらがなのメソッドを呼び出す
+あいさつする#=> "はろー、わーるど！"
+
+def add(a,b)
+  #returnも使えるが、使わないほうが主流
+  return a + b
+end
+add(1,2)#=> 3
+
+def greeting(country)
+#"こんにちは"または"hello"がメソッドの戻り値になる
+  if country == 'japan'
+    'こんにちは'
+  else
+    'hello'
+  end
+end
+greeting('japan')#=>"こんにちは"
+greeting('us') #=>"hello"
+
+def greeting(country)
+#countryがnilならメッセージを返してメソッドを抜ける
+#(nil?はオブジェクトがnilの場合にtrueを返すメソッド)
+return 'countryを入力してください'if country.nil?
+
+  if country == 'japan'
+    'こんにちは'
+  else
+    'hello'
+  end
+end
+greeting(nil) #=>"countryを入力してください"
+greeting('japan')#=>"こんにちは"
+
+#メソッド定義における引数の()
+#引数がない場合は()を付けないほうが主流
+def greeting
+  'こんにちは'
+end
+
+#()を付けても良いが、省略されることが多い
+def greeting()
+  'こんにちは'
+end
