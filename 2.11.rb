cc21f9c9e2212fbd4198eb931b66596c14bcec82
rotosiri-zu <hongjitufang25@gmail.com>
2021-07-04 22:36:01 +0900

    2.11.2 ?で終わるメソッド

--- 2.11.rb
+++ 2.11.rb
@@ -31,3 +31,31 @@ end
 
 foo #=>time: 2017-05-10 09:16:35+900,message:BAR
 
+#2.11.2 ?で終わるメソッド
+
+#空文字列であればtrue, そうでなければ false
+".empty?" #=>true
+'abc'.empty? #=>false
+
+#引数の文字列が含まれていればtrue、そうでなければfalse
+'watch'.include?('at') #=>true
+'watch'.include?('in') #=>false
+
+#奇数ならtrue、偶数ならfalse
+1.odd? #=>true
+2.odd? #=>false
+
+#オブジェクトがnilであればtrue、そうでなければ false
+nil.nil? #=>true
+'abc.nil' #=>false
+1.nil? #=>false
+
+#3の倍数なら true、それ以外はfalseを返す
+def multiple_of_three?(n)
+  n % 3 == 0
+end
+multiple_of_three?(4) #=>false
+multiple_of_three?(5) #=>false
+multiple_of_three?(6) #=>true
+
+
