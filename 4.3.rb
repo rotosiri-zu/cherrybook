2a84d1e2706074e73ad6af46d56ad19f408697c4
rotosiri-zu <hongjitufang25@gmail.com>
2021-07-24 22:49:54 +0900

    4.3 ブロック

---
+++ 4.3.rb
@@ -0,0 +1,16 @@
+# 4.3 ブロック
+# 参考 : javeの繰り返し処理
+int numbers[] = {1,2,3,4};
+int sum = 0
+for(int i = 0;i<numbers.length;i++)
+  {
+    sum+= numbers[i];
+  }
+  System.out.println(sum);//=>10
+
+int numbers[] = {1,2,3,4}
+int sum = 0;
+for(int n;numbers){
+  sum += n;
+}
+System.out.println(sum);//=>10
